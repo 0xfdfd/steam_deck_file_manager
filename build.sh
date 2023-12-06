46bb79db513e19fc5e7dbc4f4ff61b3346a2ce86
qgymib <qgymib@gmail.com>
2023-12-06 21:58:36 +0800

    add option to build as release and serve file

--- build.sh
+++ build.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+CARGO_BUILD_FLAGS=""
+WASM_PACK_BUILD_FLAGS="--dev"
+RUN=false
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "build.sh --release"
+      echo ""
+      echo "  --release: Build with --release"
+      exit 0
+      ;;
+
+    --release)
+      shift
+      CARGO_BUILD_FLAGS="--release"
+	  WASM_PACK_BUILD_FLAGS="--release"
+      ;;
+
+    --run)
+      shift
+      RUN=true
+      ;;
+
+  esac
+done
+
 # Install dependency
 if ! [ -x "$(command -v wasm-opt)" ]; then
   cargo install wasm-opt
@@ -12,10 +39,15 @@ if ! [ -x "$(command -v wasm-bindgen)" ]; then
 fi
 
 # Build frontend
-(cd frontend && cargo fmt && wasm-pack build --target web)
-
+(cd frontend && cargo fmt && wasm-pack build ${WASM_PACK_BUILD_FLAGS} --target web)
 cp frontend/pkg/frontend.js backend/assets/
 cp frontend/pkg/frontend_bg.wasm backend/assets/
 
 # Build backend
-(cd backend && cargo fmt && cargo build --release)
+(cd backend && cargo fmt && cargo build ${CARGO_BUILD_FLAGS})
+
+# Run
+if [[ "${RUN}" = "true" ]]; then
+  (cd backend && cargo run ${CARGO_BUILD_FLAGS})
+fi
+
